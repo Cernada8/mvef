2300204e59bc25360eca77eacb537368433da06a
Cernada8 <alexcernada2017@gmail.com>
2026-05-06 10:54:31 +0200

    fix: stripe apiVersion

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -13,7 +13,7 @@ import Stripe from 'stripe'
 // ─────────────────────────────────────────────────────────────────────────────
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil',
+  apiVersion: '2025-02-24.acacia',
 })
 
 export async function POST(req: NextRequest) {
